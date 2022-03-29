3e4b5a343fea0a21482c1c57e2bf9b949badc2f3
KeremAydin98 <aydinke16@itu.edu.tr>
2022-03-29 15:24:12 +0300

    ch4

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bnt4r52ukn19#gw-jd^j8r_0!1v3h87x9zqimyijz)wu%-1xzm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
