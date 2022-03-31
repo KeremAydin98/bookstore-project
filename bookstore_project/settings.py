549eda23d72cd14ce029753831564b4e048ef02b
KeremAydin98 <aydinke16@itu.edu.tr>
2022-03-31 19:17:55 +0300

    ch9

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -157,7 +157,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ACCOUNT_SESSION_REMEMBER = True
 
@@ -165,3 +165,5 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+
+DEFAULT_FROM_EMAIL = 'admin@kanagawabookstore.com'
