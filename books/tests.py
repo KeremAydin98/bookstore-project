2577542ab5ebcdb4e296547364fffbf41be2bb75
KeremAydin98 <aydinke16@itu.edu.tr>
2022-04-01 18:24:23 +0300

    ch13

--- books/tests.py
+++ books/tests.py
@@ -2,21 +2,23 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import Book, Review
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 
 class BookTests(TestCase):
 
     def setUp(self):
 
+        self.user = get_user_model().objects.create_user(
+            username= 'reviewuser',
+            email = 'reviewuser@email.com',
+            password='testpass123',
+        )
+        self.special_permission = Permission.objects.get(codename='special_status')
+
         self.book = Book.objects.create(
             title='Harry Potter',
             author='JK Rowling',
             price='25.00',
-        )
-
-        self.user = get_user_model().objects.create_user(
-            username= 'reviewuser',
-            email = 'reviewuser@email.com',
-            password='testpass123,'
         )
 
         self.review = Review.objects.create(
@@ -31,17 +33,30 @@ class BookTests(TestCase):
         self.assertEqual(f'{self.book.author}','JK Rowling')
         self.assertEqual(f'{self.book.price}','25.00')
 
-    def test_book_list_site(self):
+    def test_book_list_view_for_logged_in_user(self):
 
+        self.client.login(email='reviewuser@email.com',password='testpass123')
         response = self.client.get(reverse('book_list'))
 
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Harry Potter')
         self.assertTemplateUsed(response, 'books/book_list.html')
+
+    def test_book_list_view_for_logged_out_user(self):
+
+        self.client.logout()
+        response = self.client.get(reverse('book_list'))
+
+        self.assertEqual(response.status_code, 302)
+
 
     def test_book_detail_site(self):
 
+        self.client.login(email='reviewuser@email.com', password='testpass123')
+        self.user.user_permissions.add(self.special_permission)
+
         response = self.client.get(self.book.get_absolute_url())
-        no_response = self.client.get('/books/1/')
+        no_response = self.client.get('/books/12345/')
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
